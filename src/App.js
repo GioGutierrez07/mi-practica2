0465227a769c8a8a4ec873170dda59b74dc9e3d8
GioGutierrez07 <s20120005@alumnos.itsur.edu.mx>
2024-07-21 16:16:04 -0600

    Practica #2 - UseState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; //imorta bibleoteca react para trabajar 
+import Contador from './componentes/Contador'; //importa el componente contador desde la carpeta componentes 
+import CambiarImagen from './componentes/CambiarImagen'; //importa cambiarimagen desde la carpeta componentes
+import CambiarColorFondo from './componentes/CambiarColorFondo'; //importa cambiarfondo desde la carpeta 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App() { //define un componente llamado app 
+  return ( //Indica lo que el componente App debe renderizar
+    // Envuelve todos los elementos del componente App.
+    //muestra el titulo de la pantalla 
+    <div>
+      <h1>Ejemplo de useState y useEffect</h1>
+      /**contador  Renderiza el componente */
+      <Contador /> 
+      /**Renderiza el componente cambiarimagen*/
+      <CambiarImagen />
+      /**Renderiza el componente CambiarColorFondo*/
+      <CambiarColorFondo />
     </div>
   );
 }
-
+// Exporta el componente App como la predeterminada 
 export default App;
+
